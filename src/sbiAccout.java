00e13c5b6680a66acd3ab64b22f16863d8404f52
evildoomsizer <evildoomsizer@gmail.com>
2023-05-23 13:17:08 +0530

    Implemented changes

--- src/sbiAccout.java
+++ src/sbiAccout.java
@@ -1,3 +1,27 @@
 public class sbiAccout implements BankAccount{
+    private int balance;
+    public  sbiAccout(int balance)
+    {
+        this.balance=balance;
+    }
+
+    public void setBalance(int balance) {
+        this.balance = balance;
+    }
+
+    public int getBalance() {
+        return balance;
+    }
+
+    @Override
+    public boolean withdrawmoney(int money) {
+        if(this.balance>=money)
+        {
+            this.balance=this.balance-money;
+            return true;
+        }
+
+        return false;
+    }
     // addd
 }
